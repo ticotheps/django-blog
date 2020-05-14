c98be51f5dd1162d855d857ef1322f805c592a3f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-14 09:24:27 -0400

    feat(models.py): add a 'postdate' field to each blog post

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,3 +5,4 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
     body = models.TextField()
+    postdate = models.DateTimeField(auto_now_add=True, blank=True)
