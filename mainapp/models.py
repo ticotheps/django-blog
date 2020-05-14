d83e9ec83c5f2f6bb4204a73159250b8fca63a30
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-14 09:27:35 -0400

    feat(models.py): add url reverser

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -9,3 +10,6 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post', args=[str(self.id)])
