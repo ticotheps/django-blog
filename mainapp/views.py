0acec7d9a74d8b8da77fa10486f808e45e198cef
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-14 10:38:25 -0400

    fix(views.py): edit the value for 'name' in 'tag_posts()' method

--- mainapp/views.py
+++ mainapp/views.py
@@ -32,7 +32,7 @@ def post(request, id):
     return render(request, 'mainapp/post.html', {'object': post})
 
 def tag_posts(request, name):
-    name = Tag.clean(name)
+    name = name.lower()
     title = "Posts about {}".format(name)
     tag = get_object_or_404(Tag, name=name)
     posts = BlogPost.objects.filter(tags=tag)
