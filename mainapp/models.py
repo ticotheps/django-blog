78fd50f82ac7b78ebacc52994ad819466997a79d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-14 09:23:06 -0400

    feat(models.py): add a 'body' field

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,3 +4,4 @@ from django.conf import settings
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
+    body = models.TextField()
