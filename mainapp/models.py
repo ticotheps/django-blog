9568626838e26419976b78d3c95c5fc22e4ae543
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-14 10:26:04 -0400

    fix(models.py): fix 'clean()' method

--- mainapp/models.py
+++ mainapp/models.py
@@ -19,7 +19,8 @@ class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     
     def clean(self):
-        return self.name.lower()
+        self.name = self.name.lower()
+        return self.name
     
     def get_absolute_url(self):
         return reverse('tag_posts', args=[str(self.name)])
